a0fc5dd72f13a3aa7977f8ae8811525489d213c3
Shawn Roy <shawnr2324@gmail.com>
2022-01-18 09:14:06 -0500

    Add Custom Validation for conditional requirement properties.

--- MMM.Models/FinancialEntity_Form_Model.cs
+++ MMM.Models/FinancialEntity_Form_Model.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,36 +18,123 @@ namespace MMM.Models
         [Required, Range(1, int.MaxValue, ErrorMessage = "Please select a valid entity type.")]
         public int EntityTypeID { get; set; }
 
+        //Custom to check if entity type ID is Savings or Debt
+        [NotMapped]
+        public bool SavingsAndDebt
+        {
+            get { return this.EntityTypeID == 2 || this.EntityTypeID == 3; }
+        }
+
+        [NotMapped]
+        public bool Savings
+        {
+            get { return this.EntityTypeID == 2; }
+        }
+
+        [NotMapped]
+        public bool Debt
+        {
+            get { return this.EntityTypeID == 3; }
+        }
+
         [Required]
         public string UserID { get; set; }
 
         [Required]
         public decimal Balance { get; set; }
 
-        [RequiredIf("EntityTypeID", 3), DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = true)]
+        [RequiredConditional("Debt", "InterestRate"), DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = true)]
         public decimal? InterestRate { get; set; }
 
-        //Custom to check if entity type ID is Savings or Debt
-        [NotMapped]
-        public bool SavingsAndDebt
-        {
-            get { return this.EntityTypeID == 2 || this.EntityTypeID == 3;}
-        }
-
-        [RequiredIf("SavingsAndDebt", true), DataType(DataType.Date)]
+        [RequiredConditional("SavingsAndDebt", "OpenDate"), DataType(DataType.Date)]
         public DateTime? OpenDate { get; set; }
 
-        [RequiredIf("EntityTypeID", 3), DisplayFormat(DataFormatString = "{0:C0}", ApplyFormatInEditMode = true)]
+        [RequiredConditional("Debt", "MinimumPayment"), DisplayFormat(DataFormatString = "{0:C0}", ApplyFormatInEditMode = true)]
         public decimal? MinimumPayment { get; set; }
 
-        [RequiredIf("EntityTypeID", 2), DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = true)]
+        [RequiredConditional("Savings", "APY"), DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = true)]
         public decimal? APY { get; set; }
 
         [Required]
         public string EntityName { get; set; }
 
-        [RequiredIf("SavingsAndDebt", true), DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = true)]
+        [RequiredConditional("SavingsAndDebt", "InitialAmount"), DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = true)]
         public decimal? InitialAmount { get; set; }
+
+    }
+
+    //Custom Validation
+    public class RequiredConditional : ValidationAttribute
+    {
+        private string _entityTypeName { get; set; }
+        private bool _valid { get; set; }
+        private string _fieldName { get; set; }
+
+        public RequiredConditional(string entityTypeName, string fieldName)
+        {
+            _entityTypeName = entityTypeName;
+            _fieldName = fieldName;
+        }
+
+        public string GetErrorMessage() => $"{_fieldName} is required.";
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var obj = (FinancialEntity_Form_Model)validationContext.ObjectInstance;
+            if (GetPropValue(obj, _fieldName) == null)
+            {
+                switch (_entityTypeName)
+                {
+                    case "Debt":
+                        if (obj.Debt)
+                        {
+                            _valid = false;
+                        }
+                        else
+                        {
+                            _valid = true;
+                        }
+                        break;
+
+                    case "Savings":
+                        if (obj.Savings)
+                        {
+                            _valid = false;
+                        }
+                        else
+                        {
+                            _valid = true;
+                        }
+                        break;
+                    case "SavingsAndDebt":
+                        if (obj.SavingsAndDebt)
+                        {
+                            _valid = false;
+                        }
+                        else
+                        {
+                            _valid = true;
+                        }
+                        break;
+
+                    default:
+                        _valid = true;
+                        break;
+                }
+
+                if (!_valid)
+                {
+                    return new ValidationResult(GetErrorMessage(), new[] { validationContext.MemberName });
+                }
+            }
+
+            return ValidationResult.Success;
+
+        }
+        public static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
 
     }
 }
