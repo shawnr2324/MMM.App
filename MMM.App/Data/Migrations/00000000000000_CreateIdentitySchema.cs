cda6bf4b749070c9ed41822d6bb5f10b17cd7dde
sroy2 <sroy@cceionline.com>
2022-01-20 14:39:08 -0500

    Revert adding FirstName & LastName to AspNetUsers Table

--- MMM.App/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ MMM.App/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -40,9 +40,7 @@ namespace MMM.App.Data.Migrations
                     TwoFactorEnabled = table.Column<bool>(nullable: false),
                     LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
                     LockoutEnabled = table.Column<bool>(nullable: false),
-                    AccessFailedCount = table.Column<int>(nullable: false),
-                    FirstName = table.Column<string>(nullable: false),
-                    LastName = table.Column<string>(nullable:false)
+                    AccessFailedCount = table.Column<int>(nullable: false)
                 },
                 constraints: table =>
                 {
